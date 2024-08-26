cd569f6529804c069b0bbe5533e2f0d37bd13b46
Mohamed Shiha <mohamedshiha89@gmail.com>
2024-08-26 14:00:45 +0100

    Initial commit

---
+++ BlazorMosque/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorMosque.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
